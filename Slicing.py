d198357e5a45cd9de403b79fc836d0ece1379396
NyxCiph3r <151660576+PARIKAKUGOD1@users.noreply.github.com>
2025-08-02 15:55:57 +0530

    Update Slicing.py

--- Slicing.py
+++ Slicing.py
@@ -1,11 +1,12 @@
-list = []
+my_list = []
 
-for num in range(11):
-    list.append(num)
+for num in range(1, 11):
+    my_list.append(num)
 
-print("original list:", list)
-sub_list = list[:6]
+print("Original list:", my_list)
+
+sub_list = my_list[:5]
 print("Extracted first five elements:", sub_list)
-sub_list.reverse()
-print("Reversed extracted elements", sub_list)
 
+sub_list.reverse()
+print("Reversed extracted elements:", sub_list)
